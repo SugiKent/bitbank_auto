8465c54d3870ff57c38b1a6811428c5e3be37be0
SugiKent <ynmk1995@gmail.com>
2021-03-14 21:19:32 +0900

    より売りやすく

--- lib/sellable.rb
+++ lib/sellable.rb
@@ -31,11 +31,11 @@ class Sellable
       return false
     end
 
-    compare_slope(small: last_5days, small_name: '5days', small_by: -1,
+    compare_slope(small: last_5days, small_name: '5days', small_by: 1,
                   big: last_3days, big_name: '3days', big_by: 1) &&
-      compare_slope(small: last_3days, small_name: '3days', small_by: -1,
+      compare_slope(small: last_3days, small_name: '3days', small_by: 0.8,
                     big: last_1days, big_name: '1days', big_by: 1) &&
-                   compare_slope(small: last_1hour, small_name: '1hour', small_by: 1,
+                   compare_slope(small: last_1hour, small_name: '1hour', small_by: -1,
                     big: last_1days, big_name: '1days', big_by: 1)
   end
 end
